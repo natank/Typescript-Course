f61b5c558e501bde95eb2048f05ce041392044e8
Nati Kamusher <natan.kamusher@gmail.com>
2021-03-29 20:33:15 +0300

    project setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { Summary } from './Summary';
-import { ConsoleReport } from './reportTargets/ConsoleReports';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { HtmlReport } from './reportTargets/HtmlReport';
-
-// Create an object that satisifies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-// Create an instance of MatchReader
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
-summary.buildAndPrintReport(matchReader.matches);
+console.log('hi there');
