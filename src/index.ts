98cd8fe1d2b7605cd8ba567d2483f0100a5bea69
Nati Kamusher <natan.kamusher@gmail.com>
2021-04-03 11:25:12 +0300

    saved for remote

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
+import express from 'express';
 import cookieSession from 'cookie-session';
-
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/RootController';
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['fdjhld'] }));
-app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
